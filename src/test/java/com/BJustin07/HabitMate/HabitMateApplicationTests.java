b5ac157199cf4f59532f803a28025b8c7c6a8d4d
BJustin07 <baquiranjustin07@gmail.com>
2026-01-07 09:13:04 +0800

    Initial commit

---
+++ src/test/java/com/BJustin07/HabitMate/HabitMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BJustin07.HabitMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
